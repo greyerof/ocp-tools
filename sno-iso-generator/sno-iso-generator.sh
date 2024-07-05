cd8991af99b6b40211876eca68dde2fa1d3b5cf5
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2024-07-05 03:31:27 -0500

    sno-iso-generator: more doc and minor refactors.

--- sno-iso-generator/sno-iso-generator.sh
+++ sno-iso-generator/sno-iso-generator.sh
@@ -47,8 +47,10 @@ if [ -z "${OCP_VERSION}" ]; then
   exit 1
 fi
 
-ocp_version_str=$(echo "$OCP_VERSION" | tr "." -)
+oc_link="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-client-linux.tar.gz"
+ocp_install_link="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-install-linux.tar.gz"
 
+ocp_version_str=$(echo "$OCP_VERSION" | tr "." -)
 
 export CLUSTER_NAME=${CLUSTER_NAME:-"greyerof-${ocp_version_str}"}
 
@@ -60,51 +62,61 @@ if [ -d $ocp_folder ] ; then
   exit 1
 fi
 
+echo "Creating output folder $ocp_folder"
 mkdir $ocp_folder
 
 echo "Preparing iso for OCP version ${OCP_VERSION}, cluster name ${CLUSTER_NAME} in folder ${ocp_folder}."
 
+echo "Updating .baseDomain and .metadata.name from install-config.yaml file..."
 # Change (sequentially) the fields .baseDoman and .metadata.name using env vars (internal) $base_domain and (external) $CLUSTER_NAME
 if [ -z "${LOCAL_YQ}" ]; then
+  echo "Using yq from docker container docker.io/mikefarah/yq"
   cat install-config.yaml | podman run -i --rm docker.io/mikefarah/yq ".baseDomain = \"$base_domain\"" \
                           | podman run -i --rm docker.io/mikefarah/yq ".metadata.name = \"${CLUSTER_NAME}\"" > $ocp_folder/install-config.yaml
 else
+  echo "Using local yq program from $(which yq)"
   cat install-config.yaml | yq --yaml-output ".baseDomain = \"$base_domain\"" \
                           | yq --yaml-output ".metadata.name = \"${CLUSTER_NAME}\"" > $ocp_folder/install-config.yaml
 fi
 
 pushd $ocp_folder
-  curl -k https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-client-linux.tar.gz -o oc.tar.gz
+  echo "Downloading oc client from $oc_link"
+  curl -k $oc_link -o oc.tar.gz
+
   tar zxf oc.tar.gz
   chmod +x oc
 
-  curl -k https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_VERSION/openshift-install-linux.tar.gz -o openshift-install-linux.tar.gz
+  echo "Downloading openshift-install tool from $ocp_install_link".
+  curl -k $ocp_install_link -o openshift-install-linux.tar.gz
+
   tar zxvf openshift-install-linux.tar.gz
   chmod +x openshift-install
 
   ISO_URL=$(./openshift-install coreos print-stream-json | grep location | grep $ARCH | grep iso | cut -d\" -f4)
+
+  echo "Downloading template ISO from $ISO_URL"
   curl -L $ISO_URL -o rhcos-live.iso
 
   mkdir ocp
   cp install-config.yaml ocp
 
+  echo "Creating ignition config"
   ./openshift-install --dir=ocp create single-node-ignition-config
 
-  # Show current content of the ocp folder.
-  ls -la ocp/
-
+  echo "Creating final ISO image"
   coreos_installer="podman run --privileged --pull always --rm -v /dev:/dev -v /run/udev:/run/udev -v ${PWD}:/data -w /data quay.io/coreos/coreos-installer:release"
   ${coreos_installer} iso ignition embed -fi ocp/bootstrap-in-place-for-live-iso.ign rhcos-live.iso
 
   mv rhcos-live.iso rhcos-live-ocp-${OCP_VERSION}.iso
 
+  # List the DNS entries to place in /etc/hosts if no DNSmasq entries were set.
+  dns_entries="${CLUSTER_NAME}.$base_domain"
   subdomains="api api-int console-openshift-console.apps oauth-openshift.apps canary-openshift-ingress-canary.apps"
-
-  entry="10.0.2.15 ${CLUSTER_NAME}.$base_domain"
   for subdomain in $subdomains; do
-    entry+=" $subdomain.${CLUSTER_NAME}.$base_domain"
+    dns_entries+=" $subdomain.${CLUSTER_NAME}.$base_domain"
   done
-  echo $entry
+  echo "List of DNS entries for /etc/hosts (not needed if a DNS server was pre-configured): "
+  echo $dns_entries
 popd
 
 echo "Openshift $OCP_VERSION files created:"
