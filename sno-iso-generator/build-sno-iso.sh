1eb0d4a31a2cb2ee2b3737c1cae3c848fc2d165c
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2024-07-05 02:20:06 -0500

    sno-iso-generator: documented params and usage.

--- sno-iso-generator/build-sno-iso.sh
+++ sno-iso-generator/build-sno-iso.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 #
-# Creates ISO image for a SNO cluster.
+# Creates ISO image for a SNO (Single Node Openshift) clusters. Downloads the corresponding
+# "oc" and "openshift-install" tools from RH's official site and automates the steps in the
+# OCP installation guides for SNO clusters, like:
+# https://docs.openshift.com/container-platform/4.14/installing/installing_sno/install-sno-preparing-to-install-sno.html
+#
+# All the cluster names will use the domain "cnfcertlab.org" by default. Change the variable "base_domain" if a different
+# domain is needed. Needs a
+#
+# Preconditions:
+# - The file install-config.yaml must be exist in the current folder and the fields "pullSecret" and "sshKey" must be
+#   pre-populated with the user's RH pull secrets and its own ssh public key. The pulic key will be added to the sno
+#   server so it can be accessed through the "core" user for debugging.
+#
+# Dependencies:
+# - install-config.yaml: pullSecret and sshKey fields must be pre-populated.
+# - yq: by default, it will try to (pull &) use the docker container docker.io/mikefarah/yq,
+#      but can use a local yq program if LOCAL_YQ=1 is exported.
+#
 # Inputs:
-#   OCP_VERSION: env var with desired version, e.g. OCP_VERSION=4.13.4
-#   CLUSTER_NAME: (optional) cluster name. If not set, will be defaulted to greyerof-$OCP_VERSION
-#   LOCAL_YQ: (optional) value won't be read. It just tells the script to use the locally installed
+# - OCP_VERSION: env var with desired version, e.g. OCP_VERSION=4.13.4
+# - CLUSTER_NAME: (optional) cluster name. If not set, will be defaulted to greyerof-$OCP_VERSION
+# - LOCAL_YQ: (optional) value won't be read. It just tells the script to use the locally installed
 #             tool yq. This will avoid downloading the docker container.
 #
+# Outputs: the script will create a separate folder with all the artifacts needed to build
+#          the SNO ISO file. Folder name is ocp_${CLUSTER_NAME}
+#
+#
 
-# Set exit on error
+base_domain=cnfcertlab.org
+
+# Exit on error
 set -o errexit
 
 if [ -z "${OCP_VERSION}" ]; then
@@ -17,12 +40,11 @@ if [ -z "${OCP_VERSION}" ]; then
 fi
 
 ocp_version_str=$(echo "$OCP_VERSION" | tr "." -)
-base_domain=cnfcertlab.org
+
 
 export CLUSTER_NAME=${CLUSTER_NAME:-"greyerof-${ocp_version_str}"}
 
 ARCH=${ARCH:-x86_64}
-
 
 ocp_folder="ocp_${CLUSTER_NAME}"
 if [ -d $ocp_folder ] ; then
@@ -60,6 +82,7 @@ pushd $ocp_folder
 
   ./openshift-install --dir=ocp create single-node-ignition-config
 
+  # Show current content of the ocp folder.
   ls -la ocp/
 
   coreos_installer="podman run --privileged --pull always --rm -v /dev:/dev -v /run/udev:/run/udev -v ${PWD}:/data -w /data quay.io/coreos/coreos-installer:release"
